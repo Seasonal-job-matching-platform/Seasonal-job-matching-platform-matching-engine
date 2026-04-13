7761e3b50f01cd3d16b76dc38df478a9043e7e09
Ahmed Bahig <ahmedbahig2003@gmail.com>
2026-04-14 01:33:02 +0200

    remove venv and clean repo structure

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 
 # === DATABASE CREDENTIALS (user requested to include them here) ===
-DATABASE_URL = "postgres://u8c6vf5b77entb:p8e1c9035f0ebf2f10c378ea686550969292451793adfb2fe1ffa5af62840d50a@c2ath2egdsh9dm.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d5183gpg7pokjv"
+DATABASE_URL = "postgres://u8c6vf5b77entb:pbc4e019489c5dcc3be5e501aed4fba46b7727ad545eddf4f93a921fce710c9fa@c2ath2egdsh9dm.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d5183gpg7pokjv"
 
 # Convenience fields (not required)
 PROD_DB_HOST = "c2ath2egdsh9dm.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com"
